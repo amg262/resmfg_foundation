5e01ea489822ef4ba78d773a649f7e4ba880c9f1
Andy Gunn <andrewmgunn26@gmail.com>
2016-03-03 14:54:46 -0600

    adds IE support

--- header.php
+++ header.php
@@ -13,6 +13,9 @@
 <html class="no-js" <?php language_attributes(); ?> >
 	<head>
 		<meta charset="<?php bloginfo( 'charset' ); ?>" />
+		<?php global $is_IE; if ( $is_IE ) : ?>
+		<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
+		<?php endif; ?>
 		<meta name="viewport" content="width=device-width, initial-scale=1.0" />
 		<link rel="icon" href="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/icons/favicon.ico" type="image/x-icon">
 		<link rel="apple-touch-icon" sizes="144x144" href="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/icons/apple-touch-icon-144x144.png">
@@ -33,38 +36,42 @@
 	<?php do_action( 'foundationpress_layout_start' ); ?>
 
 	<header id="masthead" class="site-header" role="banner">
-		<div class="title-bar" data-responsive-toggle="site-navigation">
-			<button class="menu-icon" type="button" data-toggle="mobile-menu"></button>
-			<div class="title-bar-title">
-				<a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a>
+		<div data-sticky-container>
+		  <div class="sticky" id="sticky" data-sticky data-margin-top="0" style="width:100%;" data-margin-bottom="0">
+				<div class="title-bar" data-responsive-toggle="site-navigation">
+					<button class="menu-icon" type="button" data-toggle="mobile-menu"></button>
+					<div class="title-bar-title">
+						<a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a>
+					</div>
+				</div>
+				<div class="top-bar-left">
+					<ul class="menu">
+
+						<?php if ( get_field( 'logo_type', 'option' ) == 'Image' ) {
+							echo 'Image';
+							$image = get_field( 'logo_image', 'option' );
+							echo '<a href='.esc_url( home_url( '/' ) ).' ><img src='.$image['url'].' alt=='.$image['alt'].' width="250" height="auto" /></a>';
+						} else if ( get_field( 'logo_type', 'option' ) == 'Text' ) {
+							echo 'text';
+
+						} else { ?>
+							<li class="home"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></li>
+						<?php } ?>
+
+					</ul>
+				</div>
+				<nav id="site-navigation" class="main-navigation top-bar" role="navigation">
+					
+					<div class="top-bar-right">
+						<?php foundationpress_top_bar_r(); ?>
+
+						<?php if ( ! get_theme_mod( 'wpt_mobile_menu_layout' ) || get_theme_mod( 'wpt_mobile_menu_layout' ) == 'topbar' ) : ?>
+							<?php get_template_part( 'template-parts/mobile-top-bar' ); ?>
+						<?php endif; ?>
+					</div>
+				</nav>
 			</div>
 		</div>
-		<div class="top-bar-left">
-			<ul class="menu">
-
-				<?php if ( get_field( 'logo_type', 'option' ) == 'Image' ) {
-					echo 'Image';
-					$image = get_field( 'logo_image', 'option' );
-					echo '<a href='.esc_url( home_url( '/' ) ).' ><img src='.$image['url'].' alt=='.$image['alt'].' width="250" height="auto" /></a>';
-				} else if ( get_field( 'logo_type', 'option' ) == 'Text' ) {
-					echo 'text';
-
-				} else { ?>
-					<li class="home"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></li>
-				<?php } ?>
-
-			</ul>
-		</div>
-		<nav id="site-navigation" class="main-navigation top-bar" role="navigation">
-			
-			<div class="top-bar-right">
-				<?php foundationpress_top_bar_r(); ?>
-
-				<?php if ( ! get_theme_mod( 'wpt_mobile_menu_layout' ) || get_theme_mod( 'wpt_mobile_menu_layout' ) == 'topbar' ) : ?>
-					<?php get_template_part( 'template-parts/mobile-top-bar' ); ?>
-				<?php endif; ?>
-			</div>
-		</nav>
 	</header>
 
 	<section class="container">
