7c42afb90bc82c508e08de3c08e2ce92e0338bd6
Andy Gunn <andrewmgunn26@gmail.com>
2016-03-09 22:29:16 -0600

    going to move content block to subhead area

--- page-templates/platypus-front.php
+++ page-templates/platypus-front.php
@@ -74,32 +74,32 @@ get_header(); ?>
 	</div>
 
 </header>
-
-<?php do_action( 'foundationpress_before_content' ); ?>
-<?php while ( have_posts() ) : the_post(); ?>
+<!--
+<?php //do_action( 'foundationpr<!--ess_before_content' ); ?>
+<?php //while ( have_posts() ) : the_post(); ?>
 <section class="intro" role="main">
 	<div class="fp-intro">
 
 		<div <?php post_class() ?> id="post-<?php the_ID(); ?>">
-			<?php do_action( 'foundationpress_page_before_entry_content' ); ?>
+			<?php //do_action( 'foundationpress_page_before_entry_content' ); ?>
 			<div class="entry-content">
 				<?php the_content(); ?>
 			</div>
 			<footer>
-				<?php wp_link_pages( array('before' => '<nav id="page-nav"><p>' . __( 'Pages:', 'foundationpress' ), 'after' => '</p></nav>' ) ); ?>
+				<?php //wp_link_pages( array('before' => '<nav id="page-nav"><p>' . __( 'Pages:', 'foundationpress' ), 'after' => '</p></nav>' ) ); ?>
 				<p><?php the_tags(); ?></p>
 			</footer>
-			<?php do_action( 'foundationpress_page_before_comments' ); ?>
+			<?php //do_action( 'foundationpress_page_before_comments' ); ?>
 			<?php comments_template(); ?>
-			<?php do_action( 'foundationpress_page_after_comments' ); ?>
+			<?php //do_action( 'foundationpress_page_after_comments' ); ?>
 		</div>
 
 	</div>
 
 </section>
-<?php endwhile;?>
-<?php do_action( 'foundationpress_after_content' ); ?>
-
+<?php //endwhile;?>
+<?php //do_action( 'foundationpress_after_content' ); ?>
+-->
 <div class="section-divider">
 	<!--<hr />-->
 </div>
